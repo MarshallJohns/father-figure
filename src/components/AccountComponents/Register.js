fd3cf68f03b6425d2cf9cfdec3aabe80a03b0f4c
Marshall Johnson <marshalljohnson193@gmail.com>
2020-10-09 02:29:34 -0600

    init finish

--- src/components/AccountComponents/Register.js
+++ src/components/AccountComponents/Register.js
@@ -31,53 +31,58 @@ function Register(props) {
     }
 
     return (
-        <div className='register-container'>
-            { count === 1 ?
-                <div>
+        <div className='register'>
+            <div className='register-container'>
+                <h1>Father Figure</h1>
+                {count === 1 ?
+                    <div>
+                        <div className='input'>
+                            <div>First Name:</div>
+                            <input value={firstName} type='text' onChange={(e) => setFirstName(e.target.value)} />
+                        </div>
+                        <div className='input'>
+                            <div>Last Name:</div>
+                            <input value={lastName} type='text' onChange={(e) => setLastName(e.target.value)} />
+                        </div>
+                        <div className='input'>
+                            <div>Email:</div>
+                            <input value={email} type='email' onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <button onClick={() => setCount(count + 1)}>Next</button>
+                    </div>
+                    :
+                    null}
+                {count === 2 ? <div>
                     <div className='input'>
-                        <labe>First Name:</labe>
-                        <input value={firstName} type='text' onChange={(e) => setFirstName(e.target.value)} />
+                        <div>Password:</div>
+                        <input value={password} type='password' onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className='input'>
-                        <label>Last Name:</label>
-                        <input value={lastName} type='text' onChange={(e) => setLastName(e.target.value)} />
+                        <div>Confirm Password:</div>
+                        <input value={confirm} type='password' onChange={(e) => setConfirm(e.target.value)} />
                     </div>
+                    <div className='btn-container'>
+                        <button onClick={() => setCount(count - 1)}>Back</button>
+                        <button onClick={() => setCount(count + 1)}>Next</button>
+                    </div>
+                </div> :
+                    null}
+                {count === 3 ? <div>
                     <div className='input'>
-                        <label>Email:</label>
-                        <input value={email} type='email' onChange={(e) => setEmail(e.target.value)} />
+                        <div>Zipcode:</div>
+                        <input value={zipcode} type='text' onChange={(e) => setZipcode(e.target.value)} />
                     </div>
-                    <button onClick={() => setCount(count + 1)}>Next</button>
-                </div>
-                :
-                null}
-            {count === 2 ? <div>
-                <div className='input'>
-                    <label>Password:</label>
-                    <input value={password} type='password' onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <div className='input'>
-                    <label>Confirm Password:</label>
-                    <input value={confirm} type='password' onChange={(e) => setConfirm(e.target.value)} />
-                </div>
-                <button onClick={() => setCount(count - 1)}>Back</button>
-                <button onClick={() => setCount(count + 1)}>Next</button>
-            </div> :
-                null}
-            {count === 3 ? <div>
-                <div className='input'>
-                    <label>Zipcode:</label>
-                    <input value={zipcode} type='text' onChange={(e) => setZipcode(e.target.value)} />
-                </div>
 
-                <div className='input'>
-                    <label>Profile Picture:</label>
-                    <input value={profile_picture} type='text' onChange={(e) => setProfile_picture(e.target.value)} />
-                </div>
-                <button onClick={() => setCount(count - 1)}>Back</button>
-                <button onClick={() => handleSubmit()} type='submit'>Submit</button>
-            </div> : null}
+                    <div className='input'>
+                        <div>Profile Picture:</div>
+                        <input value={profile_picture} type='text' onChange={(e) => setProfile_picture(e.target.value)} />
+                    </div>
+                    <button onClick={() => setCount(count - 1)}>Back</button>
+                    <button onClick={() => handleSubmit()} type='submit'>Submit</button>
+                </div> : null}
 
-            <p>{`Step: ${count}/3`}</p>
+                <h3>{`Step: ${count}/3`}</h3>
+            </div>
         </div>
     )
 }
